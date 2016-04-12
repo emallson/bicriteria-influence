ce068f756d71a43d6878301a56070106b04811c9
Alan Kuhnle <akuhnle418@gmail.com>
2016-04-12 21:33:46 +0000

    working on oracles

--- influence_oracles.cpp
+++ influence_oracles.cpp
@@ -1,16 +1,23 @@
 #include <igraph.h>
 #include <vector>
 #include <unordered_set>
+#include <random>
+#include <algorithm>
 
 using namespace std;
 typedef unsigned long myint;
 typedef unordered_set< myint >  uset;
 
+struct mypair {
+  myint first;
+  myint second;
+};
 
 class influence_oracles {
 public:
   myint ell;
   myint k;
+  myint n;
 
   vector< igraph_t* > v_instances;
 
@@ -21,7 +28,7 @@ public:
   void estimate_influence() {}
 
   void merge_sketches(vector< myint >& sketch_1, vector< myint >& sketch_2, 
-		      vector< myint >& result ) {}p
+		      vector< myint >& result ) {}
 
   influence_oracles( vector< igraph_t* > in_instances, myint in_ell, myint in_k ) :
     v_instances( in_instances ), ell( in_ell ), k( in_k ) { }
@@ -30,16 +37,39 @@ public:
 
 void influence_oracles::compute_oracles() {
   // create the permutation of size n*ell
+  myint K = n * ell;
+  vector< mypair > perm;
+  perm.reserve( K );
+  for (myint u = 0; u < n; ++u) {
+    for (myint i = 0; i < ell; ++i) {
+
+      mypair tmp;
+      tmp.first = u;
+      tmp.second = i;
+      perm.push_back( tmp );
+    }
+  }
+
+  random_shuffle( perm.begin(), perm.end() );
 
   // group ranks by instance
+  vector< vector < mypair > > instanceRanks;
+  for (myint r = 1; r <= K; ++r ) {
+    myint i = perm[ r - 1 ].second;
+    mypair tmp;
+    tmp.first = r;
+    tmp.second = perm[ r - 1 ].first;
+    instanceRanks[i].push_back( tmp );
+  }
 
   // compute combined bottom-k rank sketches
-
+  
   // for each instance i
-
-  // for each vertex u in i
-
-  // Run BFS in instance i from u until sketch is of
+  for (myint i = 0; i < ell; ++i) {
+    // for each vertex u in i by increasing rank
+    for (myint j = 0; j < n; ++j) {
+      myint vertex = instanceRanks[i].second;
+      // Run BFS in instance i from u until sketch is of
   // size k, or BFS ends
 
   // merge the reachability sketch in instance i
