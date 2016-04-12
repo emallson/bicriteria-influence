eed8ed2e594f060bff75b210819d3f9e66425fad
Alan Kuhnle <akuhnle418@gmail.com>
2016-04-12 15:46:15 -0400

    changes

--- influence_oracles.cpp
+++ influence_oracles.cpp
@@ -1,25 +1,48 @@
 #include <igraph.h>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
+typedef unsigned long myint;
+typedef unordered_set< myint >  uset;
+
 
 class influence_oracles {
 public:
-  unsigned ell;
-  unsigned k;
+  myint ell;
+  myint k;
 
   vector< igraph_t* > v_instances;
 
-  vector< vector< unsigned > > v_sketches;
+  vector< vector< myint > > v_sketches;
 
-  void compute_oracles() {}
+  void compute_oracles();
   
   void estimate_influence() {}
 
-  void merge_sketches(vector< unsigned >& sketch_1, vector< unsigned >& sketch_2, 
-		      vector< unsigned >& result ) {}
+  void merge_sketches(vector< myint >& sketch_1, vector< myint >& sketch_2, 
+		      vector< myint >& result ) {}p
 
-  influence_oracles( vector< igraph_t* > in_instances, unsigned in_ell, unsigned in_k ) :
+  influence_oracles( vector< igraph_t* > in_instances, myint in_ell, myint in_k ) :
     v_instances( in_instances ), ell( in_ell ), k( in_k ) { }
 
 };
+
+void influence_oracles::compute_oracles() {
+  // create the permutation of size n*ell
+
+  // group ranks by instance
+
+  // compute combined bottom-k rank sketches
+
+  // for each instance i
+
+  // for each vertex u in i
+
+  // Run BFS in instance i from u until sketch is of
+  // size k, or BFS ends
+
+  // merge the reachability sketch in instance i
+  // into the global sketch. 
+
+}
--- main.cpp
+++ main.cpp
@@ -9,13 +9,18 @@ int main(void)
   igraph_rng_seed(igraph_rng_default(), 42);
   igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNP, 1000, 5.0/1000,
 			  IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
-  igraph_diameter(&graph, &diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
-  printf("Diameter of a random graph with average degree 5: %d\n",
-	 (int) diameter);
+
 
   vector< igraph_t* > in_inst(2, &graph);
 
   influence_oracles my_oracles( in_inst, 2, 2 );
+
+  igraph_t* g_pointer = (my_oracles.v_instances)[0];
+
+  igraph_diameter( g_pointer, &diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
+
+  printf("Diameter of a random graph with average degree 5: %d\n",
+	 (int) diameter);
 
   igraph_destroy(&graph);
   return 0;
